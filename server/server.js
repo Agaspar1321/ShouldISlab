e3fadc77d5758089b5d6b394d5bed03cb29b315d
AJ Gaspar <ajgaspar13@gmail.com>
2026-07-22 17:39:44 -0700

    Prep for deploy: PORT env var, start script, Node engine

--- server/server.js
+++ server/server.js
@@ -183,7 +183,8 @@ app.get('/api/verdict', (req, res) => {
     res.json({ result, comps });
 });
 
-app.listen(5000, '127.0.0.1', () => {
-    console.log('Server is running on http://127.0.0.1:5000')
+const PORT = proccess.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
 });
 
